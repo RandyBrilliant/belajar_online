07138af746b395da499432296fa07c8619683c34
Randy Brilliant <randybrilliant68@gmail.com>
2020-08-04 09:42:14 +0700

    Profile Page Complete

--- belajar_online/urls.py
+++ belajar_online/urls.py
@@ -18,11 +18,25 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from user import views as user_view
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('register/', user_view.RegisterPage, name="register"),
     path('login/', auth_views.LoginView.as_view(template_name="user/login.html"), name="login"),
-    path('login/', auth_views.LoginView.as_view(template_name="user/login.html"), name="login"),
-    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path('logout/', user_view.logoutPage, name="logout"),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name="user/password_reset.html"), name="password_reset"),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="user/password_reset_done.html"), name="password_reset_done"),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="user/password_reset_confirm.html"), name="password_reset_confirm"),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name="user/password_reset_complete.html"), name="password_reset_complete"),
+    path('register/', user_view.RegisterPage, name="register"),
+    path('profile/settings/', user_view.profile, name="profile"),
+    path('profile/password-change/', auth_views.PasswordChangeView.as_view(template_name="user/password_change.html"), name="password-change"),
+    path('profile/password-change-done/', auth_views.PasswordChangeDoneView.as_view(template_name="user/password_change_done.html"), name="password-change-done"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
